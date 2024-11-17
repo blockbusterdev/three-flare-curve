d12048caa06ca89ce2e76f750ebda381669bd3b2
DenysOstroverkh <Beautifuldevsworld@gmail.com>
2024-11-16 22:23:35 -0800

    inital build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        "postcss-loader",
+        {
+          loader: "resolve-url-loader",
+          options: {
+            sourceMap: true,
+          },
+        },
+        "sass-loader",
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
