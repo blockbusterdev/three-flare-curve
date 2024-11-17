8f1b8e26013fa50c5a9236ee436aaa74289c3fd5
DenysOstroverkh <Beautifuldevsworld@gmail.com>
2024-11-16 23:13:30 -0800

    to deploy on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        "css-loader",
-        "postcss-loader",
-        {
-          loader: "resolve-url-loader",
-          options: {
-            sourceMap: true,
-          },
-        },
-        "sass-loader",
-      ],
-    });
-    return config;
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: "480px",
+        xs2: "360px",
+        xs3: "0px",
+      },
+      spacing: {
+        "1em": "1em",
+        "2em": "2em",
+        "3em": "3em",
+        "11em": "11em",
+        "0.5em": "0.5em",
+        auto: "auto",
+      },
+      flexGrow: {
+        "100": "100",
+        "1": "1",
+      },
+    },
   },
+  plugins: [],
 };
 
 export default nextConfig;
